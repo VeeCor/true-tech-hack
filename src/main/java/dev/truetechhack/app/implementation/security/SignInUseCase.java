4c4c123d028591eafc9bb121cedf4ce5279422e3
VeeCor <corbat.p.s.221b@gmail.com>
2025-04-20 01:57:33 +0300

    Edit user authentication

--- src/main/java/dev/truetechhack/app/implementation/security/SignInUseCase.java
+++ src/main/java/dev/truetechhack/app/implementation/security/SignInUseCase.java
@@ -5,34 +5,36 @@ import dev.truetechhack.app.api.security.SignInInbound;
 import dev.truetechhack.app.exception.ProductException;
 import dev.truetechhack.domain.response.ApiResponse;
 import dev.truetechhack.domain.response.DataWrapper;
+import dev.truetechhack.domain.response.RecordsWrapper;
 import dev.truetechhack.domain.user.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SignInUseCase implements SignInInbound {
-    private static final String PATH = "/dstlr5pDegVHcXYZWA/records?";
+    private static final String PATH = "/dstlr5pDegVHcXYZWA/records?viewId=viwXKQJReqX5j&fieldKey=name";
     private final TrueTabsRepository trueTabsRepository;
 
     @Override
     public void execute(String email, String password) {
-        String uri = UriComponentsBuilder.fromPath(PATH)
-            .queryParam("email", email)
-            .queryParam("password", password)
-            .build()
-            .toUriString();
-
-        trueTabsRepository.get(uri,
-                new ParameterizedTypeReference<ApiResponse<DataWrapper<User>>>() {
-                }).doOnNext(response -> {
+        trueTabsRepository.get(PATH,
+                new ParameterizedTypeReference<ApiResponse<DataWrapper<User>>>() {})
+            .flatMap(response -> {
                 log.info("code = {}, success = {}, message = {}", response.getCode(), response.isSuccess(), response.getMessage());
-            }).doOnError(throwable -> {
-                throw new ProductException(throwable.getMessage());
-            }).subscribe();
+                return Mono.justOrEmpty(
+                    response.getData().getRecords().stream()
+                        .map(RecordsWrapper::getFields)
+                        .filter(user -> email.equals(user.getEmail()) && password.equals(user.getPassword()))
+                        .findFirst()
+                );
+            })
+            .switchIfEmpty(Mono.error(new ProductException("Invalid email or password")))
+            .doOnNext(user -> log.info("User authenticated: {}", user))
+            .subscribe();
     }
 }
